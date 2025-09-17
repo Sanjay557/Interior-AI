a3608d37e82515d407f247d8fe663daa5bd057f2
Sanjay557 <60855947+Sanjay557@users.noreply.github.com>
2025-09-17 13:44:36 +0530

    Paypal not allowing me to have authorize access but I need to figure it out

--- app/(routes)/dashboard/buy-credits/page.jsx
+++ app/(routes)/dashboard/buy-credits/page.jsx
@@ -29,17 +29,20 @@ function BuyCredits() {
 
     const [selectedOption, setSelectedOption] = React.useState([])
   return (
-    <div>
+    <div className='mt-8'>
       <h2 className='font-bold text-2xl'>Buy more credits</h2>
       <p>Unlock endless possibilities. Buy More Credits</p>
 
       <div className='grid grid-cols-2 md:grid-cols-3 
-      lg:grid-cols-4 xl:grid-cols-5 gap-4'>
+      lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-10'>
         {creditOptions.map((item, index)=> (
-            <div key={index} className='border-2 border-purple-500 p-4'>
+            <div key={index} className={`border-2 p-4 
+            flex justify-center items-center flex-col rounded-lg
+            ${selectedOption?.credits==item.credits &&'border-purple-500 border-2'}
+            `}>
                 <h2 className='font-bold text-3xl'>{item.credits}</h2>
                 <h2 className='font-medium text-xl'>Credits</h2>
-                <button className='bg-slate-400 w-full mt-4 mb-4 p-2' onClick={()=> setSelectedOption(item)}>Buy</button>
+                <button className='bg-purple-400 w-full mt-4 mb-4 p-2' onClick={()=> setSelectedOption(item)}>Buy</button>
                 
                 <h2 className='font-medium text-purple-500'>${item.amount}</h2>
             </div>
